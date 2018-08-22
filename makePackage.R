323ca7fa92be936ce72ea113cfc6a89112226c9c
danielturek <danielturek@gmail.com>
2018-08-22 14:42:13 -0400

    renamed to basicMCMCplots

--- makePackage.R
+++ makePackage.R
@@ -5,26 +5,26 @@ library(roxygen2)
 
 ## create empty package:
 ## (never need to do this again)
-##setwd('~/github/plotMCMC')
-##create('plotMCMC', rstudio = FALSE)
+##setwd('~/github/basicMCMCplots')
+##create('basicMCMCplots', rstudio = FALSE)
 
 ## use roxygen comments to generate man/*.Rd help files
-setwd('~/github/plotMCMC/plotMCMC')
+setwd('~/github/basicMCMCplots/basicMCMCplots')
 document()
 
 ## build the package tarball file
-setwd('~/github/plotMCMC')
-system('R CMD BUILD plotMCMC')
+setwd('~/github/basicMCMCplots')
+system('R CMD BUILD basicMCMCplots')
 
 ## use check() to "check" the package
-setwd('~/github/plotMCMC/plotMCMC')
+setwd('~/github/basicMCMCplots/basicMCMCplots')
 check('.')
 
 
 ## install the package from GitHub:
 library(devtools)
-install_github('danielturek/plotMCMC', subdir = 'plotMCMC')
-library(plotMCMC)
+install_github('danielturek/basicMCMCplots', subdir = 'basicMCMCplots')
+library(basicMCMCplots)
 
 
 
